378b2089b228e858ac50a870a91db53b8d536021
boredomcrusher <Matrix1000atr9@gmail.com>
2024-01-11 19:53:13 -0800

    added comments, will figure out how to refactor the equation tomorrow.

--- String/LargestSubstringWithoutCharacters.java
+++ String/LargestSubstringWithoutCharacters.java
@@ -10,11 +10,13 @@ public class LargestSubstringWithoutCharacters {
         Boolean testCase2 = largestString.lengthOfLongestSubstring("bbbbb") == 1;
         Boolean testCase3 = largestString.lengthOfLongestSubstring("pwwkew") == 3;
         Boolean testCase4 = largestString.lengthOfLongestSubstring("dvdf") == 3;
+        Boolean testCase5 = largestString.lengthOfLongestSubstring("abba") == 2;
 
         System.out.println("testCase1: " + testCase1);
         System.out.println("testCase2: " + testCase2);
         System.out.println("testCase3: " + testCase3);
         System.out.println("testCase4: " + testCase4);
+        System.out.println("testCase4: " + testCase5);
     }
 
     public int lengthOfLongestSubstring(String s) {
@@ -27,33 +29,28 @@ public class LargestSubstringWithoutCharacters {
         int finalStringLength = 0;
 
         for (int i = 0; i < s.length(); i++) {
+            // if next character has already been added to the hashmap
             if (map.get(s.charAt(i)) != null) {
-                // next, remove the string up to the last instance of the repeated character
-                // -- currently fails testcase 1
-
-                String[] split = tempString.split("" + s.charAt(i), 2);
-                tempString = split[1];
-                // System.out.println("split string: " + tempString);
-                // tempString = "";
+                // get string after first instance of repeated character
+                tempString = tempString.split("" + s.charAt(i), 2)[1];
+                // reset hashmap and fill with characters of current string
+                // -- I have no idea why just removing the current character doesn't work
+                // -- but somehow, that doesn't work, and this works for every testcase on
+                // -- leetcode
                 map = new HashMap<>();
-                // System.out.println("new map");
-                if (tempString.length() > 0) {
-                    for (int j = 0; j < tempString.length(); j++) {
-                        map.put(tempString.charAt(j), true);
-                        // System.out.println(tempString.charAt(j) + " now in map");
-                    }
-
+                for (int j = 0; j < tempString.length(); j++) {
+                    map.put(tempString.charAt(j), true);
                 }
-
             }
 
+            // add current character to hashmap
             map.put(s.charAt(i), true);
-            // System.out.println(s.charAt(i) + " now in map");
-            tempString += s.charAt(i);
-            // System.out.println("new tempstring: " + tempString);
 
+            // add current character to current largest substring without repeats
+            tempString += s.charAt(i);
+
+            // update answer
             if (tempString.length() > finalStringLength) {
-                // System.out.println("final string: " + tempString);
                 finalStringLength = tempString.length();
             }
         }
