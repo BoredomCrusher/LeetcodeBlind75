94369dc2fc3e0e75406be04ed7659287a594af14
boredomcrusher <Matrix1000atr9@gmail.com>
2024-01-11 16:05:30 -0800

    solution solves 407/987 testcases.

--- String/LargestSubstringWithoutCharacters.java
+++ String/LargestSubstringWithoutCharacters.java
@@ -1,5 +1,37 @@
 package String;
 
-public class LargestSubstringWithoutCharacters {
+import java.util.HashMap;
 
+public class LargestSubstringWithoutCharacters {
+    public static void main(String[] args) {
+        LargestSubstringWithoutCharacters largestString = new LargestSubstringWithoutCharacters();
+        String testString = "dvdf";
+
+        System.out.println("largest substring: " + largestString.lengthOfLongestSubstring(testString));
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() <= 1) {
+            return s.length();
+        }
+
+        HashMap<Character, Boolean> map = new HashMap<>();
+        String tempString = "";
+        String finalString = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) != null) {
+                tempString = "";
+                map = new HashMap<>();
+            }
+
+            map.put(s.charAt(i), true);
+            tempString += s.charAt(i);
+
+            if (tempString.length() > finalString.length()) {
+                finalString = tempString;
+            }
+        }
+        return finalString.length();
+    }
 }
