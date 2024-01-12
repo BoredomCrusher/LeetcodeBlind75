52bf5ea27b7b1dd276f636b1ef8b911300c8f921
boredomcrusher <Matrix1000atr9@gmail.com>
2024-01-11 16:18:28 -0800

    added testcases.

--- String/LargestSubstringWithoutCharacters.java
+++ String/LargestSubstringWithoutCharacters.java
@@ -5,9 +5,16 @@ import java.util.HashMap;
 public class LargestSubstringWithoutCharacters {
     public static void main(String[] args) {
         LargestSubstringWithoutCharacters largestString = new LargestSubstringWithoutCharacters();
-        String testString = "dvdf";
 
-        System.out.println("largest substring: " + largestString.lengthOfLongestSubstring(testString));
+        Boolean testCase1 = largestString.lengthOfLongestSubstring("abcabcbb") == 3;
+        Boolean testCase2 = largestString.lengthOfLongestSubstring("bbbbb") == 1;
+        Boolean testCase3 = largestString.lengthOfLongestSubstring("pwwkew") == 3;
+        Boolean testCase4 = largestString.lengthOfLongestSubstring("dvdf") == 3;
+
+        System.out.println("testCase1: " + testCase1);
+        System.out.println("testCase2: " + testCase2);
+        System.out.println("testCase3: " + testCase3);
+        System.out.println("testCase4: " + testCase4);
     }
 
     public int lengthOfLongestSubstring(String s) {
@@ -21,7 +28,10 @@ public class LargestSubstringWithoutCharacters {
 
         for (int i = 0; i < s.length(); i++) {
             if (map.get(s.charAt(i)) != null) {
-                tempString = "";
+                // next, remove the string up to the last instance of the repeated character
+                // -- currently fails testcase 1
+                String[] split = tempString.split("" + s.charAt(i), 2);
+                tempString = split[1];
                 map = new HashMap<>();
             }
 
