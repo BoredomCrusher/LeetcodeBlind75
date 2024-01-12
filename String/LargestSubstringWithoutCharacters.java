6020369ed1766771d2ff5569ff2fb206697c22d6
boredomcrusher <Matrix1000atr9@gmail.com>
2024-01-11 18:42:24 -0800

    figured out the solution, optimizing now.

--- String/LargestSubstringWithoutCharacters.java
+++ String/LargestSubstringWithoutCharacters.java
@@ -24,24 +24,39 @@ public class LargestSubstringWithoutCharacters {
 
         HashMap<Character, Boolean> map = new HashMap<>();
         String tempString = "";
-        String finalString = "";
+        int finalStringLength = 0;
 
         for (int i = 0; i < s.length(); i++) {
             if (map.get(s.charAt(i)) != null) {
                 // next, remove the string up to the last instance of the repeated character
                 // -- currently fails testcase 1
+
                 String[] split = tempString.split("" + s.charAt(i), 2);
                 tempString = split[1];
+                // System.out.println("split string: " + tempString);
+                // tempString = "";
                 map = new HashMap<>();
+                // System.out.println("new map");
+                if (tempString.length() > 0) {
+                    for (int j = 0; j < tempString.length(); j++) {
+                        map.put(tempString.charAt(j), true);
+                        // System.out.println(tempString.charAt(j) + " now in map");
+                    }
+
+                }
+
             }
 
             map.put(s.charAt(i), true);
+            // System.out.println(s.charAt(i) + " now in map");
             tempString += s.charAt(i);
+            // System.out.println("new tempstring: " + tempString);
 
-            if (tempString.length() > finalString.length()) {
-                finalString = tempString;
+            if (tempString.length() > finalStringLength) {
+                // System.out.println("final string: " + tempString);
+                finalStringLength = tempString.length();
             }
         }
-        return finalString.length();
+        return finalStringLength;
     }
 }
