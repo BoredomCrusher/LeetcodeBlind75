751e3a9a4dec38481e10ffb6eb7107d21218c24a
boredomcrusher <Matrix1000atr9@gmail.com>
2024-01-20 15:16:13 -0800

    solved leetcode 73.

--- Matrix/SetMatrixZeroes.java
+++ Matrix/SetMatrixZeroes.java
@@ -3,51 +3,48 @@ package Matrix;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+// LeetCode 73
 public class SetMatrixZeroes {
     public void setZeroes(int[][] matrix) {
         HashMap<Integer, ArrayList<Integer>> newZeroes = new HashMap<>();
 
-        // These double nested for loops iterate through the matrix.
+        // Fills the HashMap with an ArrayList for every row,
+        // this is the same runtime as checking if HashMap.get(row) == null
+        // while iterating through the matrix.
+        for (int i = 0; i < matrix.length; i++) {
+            newZeroes.put(i, new ArrayList<Integer>());
+        }
+
+        // Double nested for loops iterating through the matrix.
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 // Checks if the current location is a zero
                 // that existed in the original matrix.
-                if (matrix[i][j] == 0 && (newZeroes.get(i) != null && !newZeroes.get(i).contains(j))) {
-                    // Fills current row with zeroes
-                    matrix = fillZeroes(matrix, newZeroes, i, j);
-                    System.out.println("zero, row " + i + " column " + j);
+                if (matrix[i][j] == 0 && !newZeroes.get(i).contains(j)) {
+                    // Fills the current and column row with zeroes.
+                    fillZeroes(matrix, newZeroes, i, j);
                 }
             }
         }
     }
 
-    public int[][] fillZeroes(int[][] matrix, HashMap<Integer, ArrayList<Integer>> newZeroes, int initialRow,
+    public void fillZeroes(int[][] matrix, HashMap<Integer, ArrayList<Integer>> newZeroes, int initialRow,
             int initialColumn) {
+        // Fills current row with zeroes.
         for (int fillRow = 0; fillRow < matrix.length; fillRow++) {
+            // Avoids adding an original zero to the newZeroes HashMap.
             if (matrix[fillRow][initialColumn] != 0) {
                 matrix[fillRow][initialColumn] = 0;
-                if (newZeroes.get(fillRow) == null) {
-                    newZeroes.put(fillRow, new ArrayList<Integer>());
-                    newZeroes.get(fillRow).add(initialColumn);
-                } else {
-                    newZeroes.get(fillRow).add(initialColumn);
-                }
+                newZeroes.get(fillRow).add(initialColumn);
             }
         }
 
-        // Fills current column with zeroes
+        // Fills current column with zeroes.
         for (int fillColumn = 0; fillColumn < matrix[0].length; fillColumn++) {
             if (matrix[initialRow][fillColumn] != 0) {
                 matrix[initialRow][fillColumn] = 0;
-                if (newZeroes.get(initialRow) == null) {
-                    newZeroes.put(initialRow, new ArrayList<Integer>());
-                    newZeroes.get(initialRow).add(fillColumn);
-                } else {
-                    newZeroes.get(initialRow).add(fillColumn);
-                }
+                newZeroes.get(initialRow).add(fillColumn);
             }
         }
-
-        return matrix;
     }
 }
