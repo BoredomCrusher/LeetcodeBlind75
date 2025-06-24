0f7aea7c0d80fd17cec62448817b243e651748da
BoredomCrusher <Matrix1000atr9@gmail.com>
2025-06-23 18:29:56 -0700

    update.

--- Matrix/SetMatrixZeroes.java
+++ Matrix/SetMatrixZeroes.java
@@ -5,7 +5,36 @@ import java.util.HashMap;
 
 // LeetCode 73
 public class SetMatrixZeroes {
-    public void setZeroes(int[][] matrix) {
+     public void setZeroes(int[][] matrix) {
+        boolean zeroInInitialColumn = false;
+
+        for (int row = 0; row < matrix.length; row++) {
+            if (matrix[row][0] == 0)
+                zeroInInitialColumn = true;
+            for (int column = 1; column < matrix[0].length; column++) {
+                if (matrix[row][column] == 0) {
+                       matrix[row][0] = 0;
+                       matrix[0][column] = 0;
+                }
+                 
+            }
+        }
+        
+        // Needs to traverse backwards to not accidentally overwrite things,
+        // for strange reasons.
+        for (int row = matrix.length - 1; row >= 0; row--) {
+            for (int column = matrix[0].length - 1; column >= 1; column--) {
+                if (matrix[row][0] == 0 || matrix[0][column] == 0) 
+                    matrix[row][column] = 0;
+            }
+            
+            if (zeroInInitialColumn)
+                 matrix[row][0] = 0;
+        }
+    }
+
+
+    public void setZeroesBruteForce(int[][] matrix) {
         HashMap<Integer, ArrayList<Integer>> newZeroes = new HashMap<>();
 
         // Fills the HashMap with an ArrayList for every row,
@@ -22,13 +51,13 @@ public class SetMatrixZeroes {
                 // that existed in the original matrix.
                 if (matrix[i][j] == 0 && !newZeroes.get(i).contains(j)) {
                     // Fills the current and column row with zeroes.
-                    fillZeroes(matrix, newZeroes, i, j);
+                    fillZeroesBruteForce(matrix, newZeroes, i, j);
                 }
             }
         }
     }
 
-    public void fillZeroes(int[][] matrix, HashMap<Integer, ArrayList<Integer>> newZeroes, int initialRow,
+    public void fillZeroesBruteForce(int[][] matrix, HashMap<Integer, ArrayList<Integer>> newZeroes, int initialRow,
             int initialColumn) {
         // Fills current row with zeroes.
         for (int fillRow = 0; fillRow < matrix.length; fillRow++) {
