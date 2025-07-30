cc8f8aa1be3d0f149764f0cfd0439e0ad7ec0f2f
BoredomCrusher <Matrix1000atr9@gmail.com>
2025-07-29 17:16:47 -0700

    solved.

--- JavaAnswers/String/ValidPalindrome.java
+++ JavaAnswers/String/ValidPalindrome.java
@@ -2,4 +2,28 @@ package String;
 
 public class ValidPalindrome {
     // Leetcode Problem 125
+
+    // Brute force
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        String allLetters = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+        int right = s.length() - 1;
+        for (int left = 0; left <= right; left++) {
+            if (allLetters.contains(String.valueOf(s.charAt(left)))) {
+                
+                while (!allLetters.contains(String.valueOf(s.charAt(right))) && left < right) {
+                    right--;
+                } 
+                
+                if (s.charAt(left) != s.charAt(right)) {
+                    System.out.println("" + s.charAt(left) + " " + s.charAt(right));
+                    return false; 
+                }
+                right--;
+            }
+        }
+
+        return true;
+    }
 }
