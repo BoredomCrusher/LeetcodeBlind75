a4c43100fbcb7448a7f37dca5a75c42ee1a9f314
BoredomCrusher <Matrix1000atr9@gmail.com>
2025-07-29 17:21:10 -0700

    slightly optimized with hashset since string contains is much slower.

--- JavaAnswers/String/ValidPalindrome.java
+++ JavaAnswers/String/ValidPalindrome.java
@@ -1,18 +1,25 @@
 package String;
 
+import java.util.HashSet;
+
 public class ValidPalindrome {
     // Leetcode Problem 125
 
     // Brute force
-    public boolean isPalindrome(String s) {
+    public boolean isPalindromeHashSet(String s) {
         s = s.toLowerCase();
         String allLetters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+        HashSet<Character> set = new HashSet<>();
+
+        for (int i = 0; i < allLetters.length(); i++) {
+            set.add(allLetters.charAt(i));
+        }
+
         int right = s.length() - 1;
         for (int left = 0; left <= right; left++) {
-            if (allLetters.contains(String.valueOf(s.charAt(left)))) {
-                
-                while (!allLetters.contains(String.valueOf(s.charAt(right))) && left < right) {
+            if (set.contains(s.charAt(left))) {
+                while (!set.contains(s.charAt(right)) && left < right) {
                     right--;
                 } 
                 
